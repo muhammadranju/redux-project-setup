e9c739adf91698a9598845b48f64cb80eb8b05a4
Muhammad Ranju <mdranju23@gmail.com>
2026-01-23 06:16:12 +0600

    feat: Upgrade Next.js, React, and their types, and update tsconfig `jsx` to `react-jsx`.

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export default async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   // Define public routes that don't require authentication
@@ -13,7 +13,6 @@ export async function middleware(req: NextRequest) {
 
   // Retrieve the authentication token from cookies
   const token = req.cookies.get("token");
-
 
   // If no token is found, redirect to the login page
   if (!token) {
